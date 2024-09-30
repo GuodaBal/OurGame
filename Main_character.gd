f8611e74c6598415470fbab1d903d586c609ecf3
GuodaBal <az.guodabaltutyte@gmail.com>
2024-10-01 01:25:08 +0300

    Added some assets

--- Main_character.gd
+++ Main_character.gd
@@ -46,7 +46,6 @@ func attack(damage):
 
 func take_damage(damage):
 	hp -= damage
-	print_debug(hp)
 	get_parent().get_parent().get_node("UIOverlay").remove_HP()
 	if hp <= 0:
 		print_debug("You are dead")
