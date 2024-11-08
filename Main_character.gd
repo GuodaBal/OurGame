0b0267d6db08faac615cda238f13b638c8920b65
GuodaBal <az.guodabaltutyte@gmail.com>
2024-11-08 20:34:53 +0200

    arrow_fixed_a_bit

--- Main_character.gd
+++ Main_character.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1711003cfc5e8d42e27a3794b32b06bc5c4c839b48a132a876b76e80e7df0474
-size 3454
+oid sha256:5e5344711efc6c8a5861d90ea0498cd14a2390a39ad39b0e0b3cdbb0adf2ed83
+size 3815
--- Scripts/arrow.gd
+++ Scripts/arrow.gd
@@ -1,22 +1,23 @@
 extends RigidBody2D
 
+@onready var sprite:=$Sprite2D as Sprite2D
+@onready var col:=$CollisionShape2D as CollisionShape2D
 var damage = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 	#look_at(Vector2(70000, -30000))
-	#apply_central_impulse(Vector2(700000, -3000000))
+	#apply_central_impulse(Vector2(200, -200))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func _physics_process(delta: float) -> void:
+	sprite.rotation = get_angle_to(self.linear_velocity)
+	col.rotation = get_angle_to(self.linear_velocity) + deg_to_rad(90)
+	
 
 func _on_body_entered(body: Node) -> void:
-	print_debug("hit")
+	#print_debug("hit")
 	if body.is_in_group("Enemy"):
 		body.take_damage(damage, 3, position)
-	if !body.is_in_group("Player"):
-		print_debug(body)
-		queue_free()
+	queue_free()
