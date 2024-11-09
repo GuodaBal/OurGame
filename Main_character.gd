c2eb7117cce69f08757a5f84cf1858e14ddc2073
GuodaBal <az.guodabaltutyte@gmail.com>
2024-11-09 17:11:32 +0200

    arrow_update

--- Main_character.gd
+++ Main_character.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e5344711efc6c8a5861d90ea0498cd14a2390a39ad39b0e0b3cdbb0adf2ed83
-size 3815
+oid sha256:f920242bbb2b614bd236139aeb415f7aeaa6e8d7abc65e5891b48329408102c7
+size 4535
--- Scripts/arrow.gd
+++ Scripts/arrow.gd
@@ -12,12 +12,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	sprite.rotation = get_angle_to(self.linear_velocity)
-	col.rotation = get_angle_to(self.linear_velocity) + deg_to_rad(90)
-	
+	pass
+	#sprite.rotation = get_angle_to(self.linear_velocity)
+	#col.rotation = get_angle_to(self.linear_velocity) + deg_to_rad(90)
+
+func set_params(mass_center: float, inert:float):
+	center_of_mass.x = mass_center
+	inertia = inert
 
 func _on_body_entered(body: Node) -> void:
 	#print_debug("hit")
-	if body.is_in_group("Enemy"):
+	#print_debug(center_of_mass.x)
+	#print_debug(inertia)
+	if body.is_in_group("Enemy") or body.is_in_group("Player"):
 		body.take_damage(damage, 3, position)
 	queue_free()
