3dc3f526d0d590f9532809307226691753b7495e
GuodaBal <az.guodabaltutyte@gmail.com>
2024-11-10 15:42:52 +0200

    Added_enemy_sprites

--- Enemy.gd
+++ Enemy.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f45d3ec77180a79a9ef813f62cea518ab0ad94fe339e9bafe83f4ca7e407a93
-size 2389
+oid sha256:cd5576f7fb4f3e1c0ba68cb9eda4ebf37c3a7a13ca138175b7cafafaa6c8e9a6
+size 2547
