446e6aa8b9b85fbe370efca70d63e37a0ec52b30
koudoudou <150090844+koudoudou@users.noreply.github.com>
2024-11-11 03:10:58 +0200

    Movement sound

--- Main_character.gd
+++ Main_character.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:760b222094ceb33e6af17b88a43b4a56ee0f9af16acb44eaa0188ea2de4276a5
-size 4528
+oid sha256:88e72dc1dd5c5a4208108cd7a62c999a2a7b0e0049f742acec7cfbfa44ba0961
+size 5041
