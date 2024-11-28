c15d430f1913ae598c170a23928262ecbd7d7f7c
GuodaBal <az.guodabaltutyte@gmail.com>
2024-11-28 21:00:58 +0200

    nature_ability

--- Main_character.gd
+++ Main_character.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a40553cbe1d9a48fa6dd734a23b98c9bb606a6decc5b39a8875f09f5f07c988
-size 8092
+oid sha256:2d1c31f2949a381d76abc4a9e154ddba5dbe6a6bfb876e6a9132a5fddf4a120b
+size 8405
--- Scripts/spider.gd
+++ Scripts/spider.gd
@@ -5,52 +5,44 @@ const WALK_SPEED = 100
 var hp = 8
 var damage = 1
 
-#@onready var animation := $AnimationPlayer as AnimationPlayer
 @onready var gravity: int = ProjectSettings.get("physics/2d/default_gravity")
 @onready var floor_detector := $Flip/FloorDetector as RayCast2D
 @onready var obsticle_detector:= $Flip/ObsticleDetector as RayCast2D
-@onready var sprite := $Flip/Sprite2D as Sprite2D
-@onready var playerPostion = get_parent().get_node("MainCharacter").position
-@onready var flip := $Flip as Node2D
-@onready var attackTimer := $AttackTimer as Timer
-@onready var spawner := $Flip/PoisonSpawner as Node2D
-@onready var rebound := $Rebound as Area2D
+@onready var playerPosition = get_parent().get_node("MainCharacter").position
+@onready var flip := $Flip as Node2D #Node that flips sprite and raycasts
+@onready var attackTimer := $AttackTimer as Timer #How often it shoots the projectile
+@onready var spawner := $Flip/PoisonSpawner as Node2D #Projectile spawn position
+@onready var rebound := $Rebound as Area2D #Stops player from standing on it
 
 var knockback = Vector2.ZERO
-var margin = 10
-var range = 300
+var margin = 100 #How far away the player has to be to follow
+var range = 300 #Range to start following player
 var last_direction = 0
 var shoot_force = 2000
 
-func _ready() -> void:
-	#sprite_scale = sprite.scale.x
-	pass
-
 func _physics_process(delta: float) -> void:
-	playerPostion = get_parent().get_node("MainCharacter").position
+	playerPosition = get_parent().get_node("MainCharacter").position
 	velocity.y += gravity * delta
-	if abs(playerPostion.y - position.y) < range:
+	if abs(playerPosition.y - position.y) < range:
 		if floor_detector.is_colliding() and not obsticle_detector.is_colliding():
-			if abs(playerPostion.x - position.x) > margin:
-				last_direction = sign(playerPostion.x - position.x)
+			if abs(playerPosition.x - position.x) > margin:
+				last_direction = sign(playerPosition.x - position.x)
 				velocity.x = WALK_SPEED * last_direction
 				flip.scale.x = -last_direction
 			else:
 				velocity.x = 0
-		elif (last_direction > 0 and playerPostion.x - position.x < -margin) or (last_direction < 0 and playerPostion.x - position.x > margin):
+		#If can't move forward and player is in opposite direction, flip
+		elif (last_direction > 0 and playerPosition.x - position.x < -margin) or (last_direction < 0 and playerPosition.x - position.x > margin):
 			velocity.x = WALK_SPEED * -last_direction
 			flip.scale.x = last_direction
 		else:
 			velocity.x = 0
 	else:
 		velocity.x = 0
-		
 	velocity += knockback
-
 	move_and_slide()
 	knockback = lerp(knockback, Vector2.ZERO, 0.1)
 
-	
 func take_damage(damage: int, knockback_strength: int, character_position: Vector2):
 	hp-=damage
 	var direction = position - character_position
@@ -64,15 +56,17 @@ func take_damage(damage: int, knockback_strength: int, character_position: Vecto
 		queue_free()
 
 func _on_attack_timer_timeout():
-	if abs(playerPostion.y - position.y) < range:
+	#shoots poison projectile
+	if abs(playerPosition.y - position.y) < range:
 		var projectile = preload("res://tscn_files/poison_projectile.tscn").instantiate()
 		spawner.add_child(projectile)
 		projectile.apply_impulse(Vector2(last_direction, 0)*shoot_force)
+		#Guarantees player cannot stand on enemy without taking damage
 		for body in rebound.get_overlapping_bodies():
 			if body.is_in_group("Player"):
 				body.take_damage(damage, 5, position)
 
-
+#Guarantees player cannot stand on enemy without taking damage
 func _on_rebound_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		body.take_damage(damage, 5, position)
