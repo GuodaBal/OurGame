a171b4b159abbb25bba851f6181d99a38bf010d1
koudoudou <150090844+koudoudou@users.noreply.github.com>
2024-12-16 01:48:50 +0200

    SFX pt.2

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6d2dfed4dfff2e48e71502735ace18ddcad2e9991f22e64ec8bb88e124b82e4
-size 2620
+oid sha256:ff31f453d83b26a1780ecd72a54b1138fe922925520b5dfcfc533fcbfae4b167
+size 2770
