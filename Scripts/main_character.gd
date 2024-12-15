3ca5d23c1c59d19a60ffcacac539658310302089
koudoudou <150090844+koudoudou@users.noreply.github.com>
2024-12-15 23:26:06 +0200

    new

--- Scripts/main_character.gd
+++ Scripts/main_character.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d2405c73cabb35d840ce29a4de2621dce27bcd07a09040ded90c315a79936b5
-size 10568
+oid sha256:6dd57b3758e3da8272738ea37f3de5194c80a71c1e17b349b6d899435d1c5fe9
+size 260
