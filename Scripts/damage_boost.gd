8356af67923e3e0c0ab002941d1a71f7fe4ad886
koudoudou <150090844+koudoudou@users.noreply.github.com>
2024-12-16 18:46:08 +0200

    1

--- Scripts/damage_boost.gd
+++ Scripts/damage_boost.gd
@@ -1,10 +1,11 @@
 extends Area2D
 
 @onready var animation := $AnimatedSprite2D as AnimatedSprite2D
-
+@onready var audio = $AudioStreamPlayer
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		animation.play("open")
+		AudioManager.play_with_random_pitch(audio)		
 		await animation.animation_finished
 		body.attack_boost()
 		DialogueManager.show_dialogue_balloon(load("res://Dialogue/ability.dialogue"), "damage_boost")
